114c7d7aa39132c990b7933127458f44f35ef35e
Rita.Yang <rita80221@hotmail.com>
2017-09-24 16:38:12 +0800

    SOLID example edit

--- SOLID/SRP/Movie.php
+++ SOLID/SRP/Movie.php
@@ -39,28 +39,6 @@ interface Package{
 interface Pay{
     public function pay();
 }
-class Tickets implements Ticket, Pay{
-    public function orderTickets(){
-        //訂票
-    }
-    public function pay(){
-        //付款
-    }
-    public function getTickets(){
-        //取票
-    }
-}
-class Packages implements Package, Pay{
-    public function orderPackages(){
-        //訂餐
-    }
-    public function pay(){
-        //付款
-    }
-    public function getPackages(){
-        //取餐
-    }
-}
 class TicketsAndPackages implements Ticket, Package, Pay{
     public function orderTickets(){
         //訂票
