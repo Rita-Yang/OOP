3ab84c1354907adeaa15d18d4c7855dbfc3138cb
Rita.Yang <rita80221@hotmail.com>
2017-12-08 00:05:05 +0800

    simpleFactory example

---
+++ designPattern/simpleFactory/abstractMethodAndDiscount.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RitaYang
+ * Date: 2017/12/7
+ * Time: 下午10:42
+ */
+
+abstract class abstractMethodAndDiscount
+{
+    public abstract function discount();
+}
+
+class openingMinus extends abstractMethodAndDiscount{
+
+    private $price;
+    private $coupons = 200;
+
+    public function __construct($price)
+    {
+        $this->price = $price;
+    }
+
+    public function discount()
+    {
+        if($this->price > $this->coupons)
+            return $this->price - $this->coupons;
+        else
+            return 0;
+    }
+}
+
+class openingMultiply extends abstractMethodAndDiscount{
+
+    private $price;
+    private $coupons = 0.85;
+
+    public function __construct($price)
+    {
+        $this->price = $price;
+    }
+
+    public function discount()
+    {
+        if($this->price >= 100)
+            return $this->price * $this->coupons;
+        else
+            return 0;
+    }
+}
---
+++ designPattern/simpleFactory/discountSimpleFactory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RitaYang
+ * Date: 2017/12/7
+ * Time: 下午10:39
+ */
+
+class discountSimpleFactory
+{
+    public static function selectDiscount($method, $price){
+
+        switch ($method){
+            case '開幕折抵兩百元':
+                return new openingMinus($price);
+                break;
+            case '開幕折扣八五折':
+                return new openingMultiply($price);
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+}
