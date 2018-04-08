9abd23ea5c8bf57d6dd3bed62da3a9bcf1e745a2
Rita.Yang <rita80221@hotmail.com>
2018-04-08 22:10:32 +0800

    UnitTest site1 update.

---
+++ UnitTest/site1/SubjectOne.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RitaYang
+ * Date: 2018/3/17
+ * Time: 下午3:08
+ */
+
+class SubjectOne{
+    public function getSquare($num1){
+        return $num1 * $num1;
+    }
+}
